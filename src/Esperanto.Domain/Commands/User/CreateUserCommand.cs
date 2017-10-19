a74cfd1ecf852586d359b47192cb1690dff329a2
gabrielcabralbfr <gabrielcabralbfr@gmail.com>
2017-10-19 18:29:44 -0200

    Started creating Commands

---
+++ src/Esperanto.Domain/Commands/User/CreateUserCommand.cs
@@ -0,0 +1,29 @@
+﻿using Esperanto.Domain.Enums.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esperanto.Domain.Commands.User
+{
+    class CreateUserCommand
+    {
+        public CreateUserCommand(string email, string password, string name, EUserRole userRole)
+        {
+            this.Email = email;
+            this.Password = password;
+            this.Name = name;
+            this.UserRole = userRole;
+            
+        }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public EUserRole UserRole { get; set; }
+    }
+}
