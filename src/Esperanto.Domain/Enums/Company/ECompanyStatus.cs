4abdf29be081b900f6ee6061f9f375b799539889
gabrielcabralbfr <gabrielcabralbfr@gmail.com>
2017-10-26 19:00:07 -0200

    Started Specs

--- src/Esperanto.Domain/Enums/Company/ECompanyStatus.cs
+++ src/Esperanto.Domain/Enums/Company/ECompanyStatus.cs
@@ -8,7 +8,14 @@ namespace Esperanto.Domain.Enums.Company
 {
     public enum ECompanyStatus
     {
+        /// <summary>
+        /// Comapny ativa do sistema
+        /// </summary>
         Active = 1,
+
+        /// <summary>
+        /// Comapny inativa do sistema
+        /// </summary>
         Inactive = 0
     }
 }
---
+++ src/Esperanto.Domain/Specs/UserSpecs.cs
@@ -0,0 +1,24 @@
+﻿using Esperanto.Domain.Entities;
+using GTL.WebStore.SharedKernel.Helpers;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Esperanto.Domain.Specs
+{
+    public static class UserSpecs
+    {
+        /// <summary>
+        /// Método para autenticar o usuário
+        /// </summary>
+        /// <param name="email">E-mail do usuário</param>
+        /// <param name="password">Senha do usuário</param>
+        /// <returns>Retorna true caso e-mail e senha estejam corretos</returns>
+        public static Expression<Func<User, bool>> AuthenticateUser(string email, string password)
+        {
+            var encriptedPassword = StringHelper.Encrypt(password);
+            return u => u.Email == email && u.Password == encriptedPassword;
+        }
+
+    }
+}
