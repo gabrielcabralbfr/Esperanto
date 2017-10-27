5706ef4195b716779970a386af9a56e2d0a0a4a2
GBNOTE\gabri <gabrielcabralbfr@gmail.com>
2017-10-27 17:42:17 -0200

    fix SharedKernel

---
+++ src/GTL.WebStore.SharedKernel/IHandler.cs
@@ -0,0 +1,17 @@
+﻿using GTL.WebStore.SharedKernel.Events.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTL.WebStore.SharedKernel
+{
+    public interface IHandler<T> : IDisposable
+        where T : IDomainEvent
+    {
+        void Handle(T args);
+        IEnumerable<T> Notify();
+        bool HasNotifications();
+    }
+}
